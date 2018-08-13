cb1ec4c5aa9fd0179c2e2d6329889bd11a399935
Pandiarajan Nagarajan <pandiarajan@ideas2it.com>
2018-08-13 16:35:13 +0530

    Android: Generate secure key and secure IV utility added

--- src/android/AES256.java
+++ src/android/AES256.java
@@ -7,7 +7,9 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.security.SecureRandom;
 import java.security.spec.KeySpec;
+import java.util.Random;
 
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
@@ -16,6 +18,8 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
 
+import shaded.org.apache.commons.codec.binary.Hex;
+
 /**
  * This class used to perform AES encryption and decryption.
  */
@@ -23,23 +27,40 @@ public class AES256 extends CordovaPlugin {
 
     private static final String ENCRYPT = "encrypt";
     private static final String DECRYPT = "decrypt";
+    private static final String GENERATE_SECURE_KEY = "generateSecureKey";
+    private static final String GENERATE_SECURE_IV = "generateSecureIV";
+
     private static final String CIPHER_TRANSFORMATION = "AES/CBC/PKCS5PADDING";
     private static final int PBKDF2_ITERATION_COUNT = 1001;
     private static final int PBKDF2_KEY_LENGTH = 256;
+    private static final int SECURE_IV_LENGTH = 64;
+    private static final int SECURE_KEY_LENGTH = 128;
     private static final String PBKDF2_ALGORITHM = "PBKDF2WithHmacSHA1";
     private static final String PBKDF2_SALT = "hY0wTq6xwc6ni01G";
+    private static final Random RANDOM = new SecureRandom();
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         try {
-            String secureKey = args.getString(0);
-            String iv = args.getString(1);
-            String value = args.getString(2);
             if (ENCRYPT.equalsIgnoreCase(action)) {
+                String secureKey = args.getString(0);
+                String iv = args.getString(1);
+                String value = args.getString(2);
                 callbackContext.success(encrypt(secureKey, value, iv));
                 return true;
             } else if (DECRYPT.equalsIgnoreCase(action)) {
+                String secureKey = args.getString(0);
+                String iv = args.getString(1);
+                String value = args.getString(2);
                 callbackContext.success(decrypt(secureKey, value, iv));
+                return true;
+            } else if (GENERATE_SECURE_KEY.equalsIgnoreCase(action)) {
+                String password = args.getString(0);
+                callbackContext.success(generateSecureKey(password));
+                return true;
+            } else if (GENERATE_SECURE_IV.equalsIgnoreCase(action)) {
+                String password = args.getString(0);
+                callbackContext.success(generateSecureIV(password));
                 return true;
             } else {
                 callbackContext.error("Invalid method call");
@@ -115,11 +136,54 @@ public class AES256 extends CordovaPlugin {
      * @see <a href="https://docs.oracle.com/javase/8/docs/api/javax/crypto/spec/PBEKeySpec.html">
      * https://docs.oracle.com/javase/8/docs/api/javax/crypto/spec/PBEKeySpec.html</a>
      */
-    public static byte[] generatePBKDF2(char[] password, byte[] salt, int iterationCount,
-                                        int keyLength) throws Exception {
+    private static byte[] generatePBKDF2(char[] password, byte[] salt, int iterationCount,
+                                         int keyLength) throws Exception {
         SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance(PBKDF2_ALGORITHM);
         KeySpec keySpec = new PBEKeySpec(password, salt, iterationCount, keyLength);
         SecretKey secretKey = secretKeyFactory.generateSecret(keySpec);
         return secretKey.getEncoded();
+    }
+
+    /**
+     * <p>
+     * This method used to generate the secure key based on the PBKDF2 algorithm
+     * </p>
+     *
+     * @param password The password
+     * @return SecureKey
+     * @throws Exception
+     */
+    private static String generateSecureKey(String password) throws Exception {
+        byte[] secureKeyInBytes = generatePBKDF2(password.toCharArray(), generateRandomSalt(),
+                PBKDF2_ITERATION_COUNT, SECURE_KEY_LENGTH);
+        return Hex.encodeHexString(secureKeyInBytes);
+    }
+
+    /**
+     * <p>
+     * This method used to generate the secure IV based on the PBKDF2 algorithm
+     * </p>
+     *
+     * @param password The password
+     * @return SecureIV
+     * @throws Exception
+     */
+    private static String generateSecureIV(String password) throws Exception {
+        byte[] secureIVInBytes = generatePBKDF2(password.toCharArray(), generateRandomSalt(),
+                PBKDF2_ITERATION_COUNT, SECURE_IV_LENGTH);
+        return Hex.encodeHexString(secureIVInBytes);
+    }
+
+    /**
+     * <p>
+     * This method used to generate the random salt
+     * </p>
+     *
+     * @return
+     */
+    private static byte[] generateRandomSalt() {
+        byte[] salt = new byte[16];
+        RANDOM.nextBytes(salt);
+        return salt;
     }
 }
--- www/AES256.js
+++ www/AES256.js
@@ -10,6 +10,14 @@ AES256.prototype.decrypt = function (secureKey, iv, value, success, error) {
     exec(success, error, 'AES256', 'decrypt', [secureKey, iv, value]);
 };
 
+AES256.prototype.generateSecureKey = function (password, success, error) {
+    exec(success, error, 'AES256', 'generateSecureKey', [password]);
+};
+
+AES256.prototype.generateSecureIV = function (password, success, error) {
+    exec(success, error, 'AES256', 'generateSecureIV', [password]);
+};
+
 var aES256 = new AES256();
 
 module.exports = aES256;
