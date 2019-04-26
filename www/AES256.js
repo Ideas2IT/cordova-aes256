7b9112c44784d90662c3eed9ea78de5428a0d6d9
Pandiarajan Nagarajan <pandiarajan@ideas2it.com>
2019-04-26 19:06:14 +0530

    Empty inputs error handling added

--- www/AES256.js
+++ www/AES256.js
@@ -3,11 +3,19 @@ var exec = require('cordova/exec');
 var AES256 = function () {};
 
 AES256.prototype.encrypt = function (secureKey, iv, value, success, error) {
-    exec(success, error, 'AES256', 'encrypt', [secureKey, iv, value]);
+    if (secureKey && iv && value) {
+        exec(success, error, 'AES256', 'encrypt', [secureKey, iv, value]);
+    } else {
+        success('');
+    }
 };
 
 AES256.prototype.decrypt = function (secureKey, iv, value, success, error) {
-    exec(success, error, 'AES256', 'decrypt', [secureKey, iv, value]);
+    if (secureKey && iv && value) {
+        exec(success, error, 'AES256', 'decrypt', [secureKey, iv, value]);
+    } else {
+        success('');
+    }
 };
 
 AES256.prototype.generateSecureKey = function (password, success, error) {
